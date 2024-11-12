1e500fc53b80efe22674dda099e24d96bab9ca05
Georgekuprashvili <kuprashviligeorge@gmail.com>
2024-11-12 20:26:34 +0400

    .

---
+++ script.js
@@ -0,0 +1,23 @@
+<div class="timer">
+      <div class="heading-1">
+        <h1>We’re launching soon</h1>
+      </div>
+      <div class="count_down">
+        <div class="days">
+          08
+          <p>DAYS</p>
+        </div>
+        <div class="hours">
+          23
+          <p>HOURS</p>
+        </div>
+        <div class="minutes">
+          55
+          <p>MINUTES</p>
+        </div>
+        <div class="seconds">
+          41
+          <p>SECONDS</p>
+        </div>
+      </div>
+    </div>
