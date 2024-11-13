9327426ed038d582aad477da2dae2c3bd6a28784
Georgekuprashvili <kuprashviligeorge@gmail.com>
2024-11-13 14:44:42 +0400

    .

--- script.js
+++ script.js
@@ -1,23 +1,11 @@
-<div class="timer">
-      <div class="heading-1">
-        <h1>We’re launching soon</h1>
-      </div>
-      <div class="count_down">
-        <div class="days">
-          08
-          <p>DAYS</p>
-        </div>
-        <div class="hours">
-          23
-          <p>HOURS</p>
-        </div>
-        <div class="minutes">
-          55
-          <p>MINUTES</p>
-        </div>
-        <div class="seconds">
-          41
-          <p>SECONDS</p>
-        </div>
-      </div>
-    </div>
+let day = prompt("შეიყვანეთ დღე");
+let hour = prompt("შეიყვანეთ საათი:");
+let minute = prompt("შეიყვანეთ წუთი");
+let second = prompt("შეიყვანეთ წამი");
+
+let days = document.getElementById("days");
+let hours = document.getElementById("hours");
+let minutes = document.getElementById("minutes");
+let seconds = document.getElementById("seconds");
+
+let time = setInterval(() => {});
