0eb7dfb67268ed9abe3d71d16c00011c30ca6ace
Georgekuprashvili <kuprashviligeorge@gmail.com>
2024-11-13 16:08:24 +0400

    .

--- script.js
+++ script.js
@@ -8,4 +8,25 @@ let hours = document.getElementById("hours");
 let minutes = document.getElementById("minutes");
 let seconds = document.getElementById("seconds");
 
-let time = setInterval(() => {});
+let interval = setInterval(() => {
+  second--;
+  if (second < 0) {
+    second = 59;
+    minute--;
+    if (minute < 0) {
+      minute = 59;
+      hour--;
+      if (hour < 0) {
+        hour = 23;
+        day--;
+        if (day < 0) {
+          clearInterval(interval);
+        }
+      }
+    }
+  }
+  days.textContent = day;
+  hours.textContent = hour;
+  minutes.textContent = minute;
+  seconds.textContent = second;
+}, 1000);
